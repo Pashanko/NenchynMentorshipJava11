35edf4166146202afeb86d5931c959c56bc90b90
Pashanko <pnenchyn@gmail.com>
2022-06-02 22:53:13 +0300

    Red bar phase

---
+++ testing/src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
@@ -0,0 +1,17 @@
+package com.epam.ld.module2.testing.template;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TemplateEngineTest {
+    @Test
+    void generateMessageValidParams(){
+        fail();
+    }
+
+    @Test
+    void generateMessageInvalidParams(){
+        fail();
+    }
+}
